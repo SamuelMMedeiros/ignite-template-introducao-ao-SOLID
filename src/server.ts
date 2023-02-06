1cd4ee36690c33ee330938d80cb09ad4720fd444
Samuel Medeiros <sammmedeiros20@gmail.com>
2023-02-05 22:20:19 -0300

    Aicionado o arquivo para documentação com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import SwaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "./swagger.json";
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
